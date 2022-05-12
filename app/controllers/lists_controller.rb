29095f58e0d8704b7249ba7bed3cfbe742e9f8c0
Kim Coquilla <kimcoquilla@gmail.com>
2022-05-12 16:55:05 -0400

    Watch List

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,35 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_param)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def list_param
+    params.require(:list).permit(:name)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Movie.destroy_all
+
+# 10.times do
+# Movie.create(title: Faker::Movie.title, overview: Faker::ChuckNorris.fact, poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: rand(5..10))
+# end
+
+# puts 'Seeds Done'
+require 'open-uri'
+require 'json'
+
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+
+# Populates Movie DB with top rated movie from API
+doc = JSON.parse(URI.open(url).read)
+doc['results'].each do |m|
+  Movie.create(title: m['title'], overview: m['overview'], poster_url: "https://image.tmdb.org/t/p/w500#{m['poster_path']}", rating: m['vote_average'])
+end
+
+puts 'Seeds Done'
